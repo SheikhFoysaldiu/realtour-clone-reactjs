06943d20d312d709204e66649dc732938616d9aa
Foysal Sheikh <foysal15-3442@diu.edu.bd>
2022-10-19 10:56:26 +0600

    Create Pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import { Route, Routes } from "react-router";
+import ForgetPassword from "./pages/forgetpassword/ForgetPassword.page";
+import Home from "./pages/home/Home.page";
+import Offer from "./pages/offers/Offer.page";
+import Profile from "./pages/profile/Profile.page";
+import SignIn from "./pages/signin/SignIn.page";
+import SignUp from "./pages/signup/SignUp.page";
+
 function App() {
 	return (
 		<>
-			<h2 className=''>hello World</h2>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/profile' element={<Profile />} />
+				<Route path='/sign-in' element={<SignIn />} />
+				<Route path='/sign-up' element={<SignUp />} />
+				<Route path='/offers' element={<Offer />} />
+				<Route path='/forget-password' element={<ForgetPassword />} />
+			</Routes>
 		</>
 	);
 }
